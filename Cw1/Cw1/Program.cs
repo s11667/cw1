7c004cd850bb67d9dba1453eac5729a4c42edbfb
Oleksander Solop <s11667@pjwstk.edu.pl>
2020-03-01 16:22:24 +0100

    Add e-mail crowler

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        emas(content);
+                    }
+                }
+            }
+        }
+
+        public static void emas(string text)
+        {
+            const string MatchEmailPattern =
+           @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+           + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+             + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+           + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})";
+            Regex rx = new Regex(MatchEmailPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            // Find matches.
+            MatchCollection matches = rx.Matches(text);
+            // Report the number of matches found.
+            int noOfMatches = matches.Count;
+            // Report on each match.
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value.ToString());
+            }
         }
     }
 }
